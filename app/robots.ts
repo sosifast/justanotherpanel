6d980f9baa58f790867a6b72da87e47298df2417
Gabriella <gabriella@Gabriella.local>
2026-02-25 15:03:03 +0700

    update

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://justanotherpanel.online'
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://www.apkey.net'
 
     return {
         rules: {
